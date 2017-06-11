e94f9f0654398cd226fca786a6689d1a1b5028fa
vishal0426 <vsahu82@gmail.com>
2017-06-11 17:52:36 +0530

    distance between locations google api

---
+++ dropshuttl_view/src/main/java/com/app/dropshuttle/googleapis/AddressLatitudeLongitude.java
@@ -0,0 +1,70 @@
+package com.app.dropshuttle.googleapis;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class AddressLatitudeLongitude {
+
+	
+	public static void main(String[] args) throws ParseException  {
+
+		String requestUrl="https://maps.googleapis.com/maps/api/geocode/json?address=";
+		String api_key="&key=AIzaSyDFuiFYl3dg1bm4xofhWp7sVE3y0dXTgag";
+		String address="Mayur%20Vihar%20Phase%201%20New%20Delhi";
+		double lat=0.0,lng=0.0;
+		try {
+			JSONParser parser = new JSONParser();
+			requestUrl=requestUrl+address+api_key;
+		    System.out.println(requestUrl);
+			URL url = new URL(requestUrl);
+			
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}
+			
+			Object obj = parser.parse(new InputStreamReader((conn.getInputStream())));;
+			
+	        JSONObject jsonObject = (JSONObject) obj;
+	        JSONArray res=null;
+	        //System.out.println(jsonObject);
+			if(jsonObject.get("results")!=null)
+	         res = (JSONArray) jsonObject.get("results");
+			@SuppressWarnings("unchecked")
+				Iterator<JSONObject> iterator = res.iterator();
+	            while (iterator.hasNext()) {
+	            	JSONObject json =iterator.next();
+	            	JSONObject json2 = (JSONObject) json.get("geometry");
+	            	JSONObject json3 = (JSONObject) json2.get("location");
+	            	lat =Double.parseDouble(json3.get("lat").toString());
+	            	lng =Double.parseDouble(json3.get("lng").toString());
+	                System.out.println("lat "+lat+" lng "+lng);
+	            }
+			 conn.disconnect();
+
+		  } catch (MalformedURLException e) {
+                      e.printStackTrace();
+
+		  } catch (IOException e) {
+
+			e.printStackTrace();
+
+		  }
+
+		}
+
+	
+}
