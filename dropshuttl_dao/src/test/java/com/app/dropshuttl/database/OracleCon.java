79b4a3743c58391257e0f658de2d3e6cb6d54c1a
vishal0426 <vsahu82@gmail.com>
2017-06-22 13:56:34 +0530

    test database connection class for oracle db

--- dropshuttl_dao/src/test/java/com/app/dropshuttl/database/OracleCon.java
+++ dropshuttl_dao/src/test/java/com/app/dropshuttl/database/OracleCon.java
@@ -23,6 +23,7 @@ System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
 //step5 close the connection object  
 con.close();  
   
+
 }catch(Exception e){ System.out.println(e);}  
   
 }  
