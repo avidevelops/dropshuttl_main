3c73276291d87eb41df55b72e02e18d2d0362d77
vishal0426 <vsahu82@gmail.com>
2017-07-02 20:55:52 +0530

    signup validation on html using angularjs

--- dropshuttl_view/src/main/webapp/js/controller/userLogin.js
+++ dropshuttl_view/src/main/webapp/js/controller/userLogin.js
@@ -11,23 +11,12 @@ app.controller('signUpCtrl', ['$scope','$http','$location',function($scope,$http
 	user.umailId=userData.umailId;
 	user.umob=userData.umob;
 	user.pass=userData.pass;
-	//var data = 'uname=' + $scope.uname + '&umailId=' + $scope.umailId + '&umob=' + $scope.umob+ '&pass= '+$scope.pass;
+	
 	var data=JSON.stringify(user);
 	
-	/*var config = {
-            headers : {
-                'Content-Type': 'application/json;'
-            }
-        }
-	*/
-//$location.path('addAccount');
-	
-	var resp=$http.post('/dropshuttl/addUser', data,{'Content-Type': 'application/json'}).then(function onSuccess(response) {
-			    // Handle success
-			 //   var user = response.data;
-			   
-			   console.log(response.status);
-	
+    var resp=$http.post('/dropshuttl/addUser', data,{'Content-Type': 'application/json'}).then(function onSuccess(response) {
+	console.log(response.status+"   "+resp);
+    $location.path('/login');
 	
 	}).catch(function onError(response) {
 	    // Handle error
@@ -41,34 +30,8 @@ app.controller('signUpCtrl', ['$scope','$http','$location',function($scope,$http
 	  });
 	
 	}
-	$scope.testForm=function(){
-		var data="";
-		var config = {
-	            headers : {
-	                'Content-Type': 'application/json;charset=utf-8;'
-	            }
-		}
-	var re=$http.post('/testcontrol', JSON.stringify(data), config).then(function onSuccess(response) {
-	    // Handle success
-	    var data = response.data;
-	    var status = response.status;
 
-
-}).catch(function onError(response) {
-// Handle error
-var data = response.data;
-var status = response.status;
-var statusText = response.statusText;
-var headers = response.headers;
-var config = response.config;
-console.log(status+" "+statusText);
-
-});
-		
-	};
-	
-
-}]);
+	}]);
 /*app.factory('dataService', function ($q, $timeout) {
     return { 
         data : {},
